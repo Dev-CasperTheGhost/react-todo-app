fde3c69b5de02ee2c08d9070cdb7e249339696f5
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-05-03 20:27:56 +0200

    Updates

---
+++ precache-manifest.2c18d54e4ab5c936410dc1d886312451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece833dc8a94e7b16a77f607d76bb349",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "fb5b901b7447d13cd607",
+    "url": "/react-todo-app/static/css/main.9b861c81.chunk.css"
+  },
+  {
+    "revision": "113483b9313e6903bb8b",
+    "url": "/react-todo-app/static/js/2.e18b5195.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo-app/static/js/2.e18b5195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5b901b7447d13cd607",
+    "url": "/react-todo-app/static/js/main.f39339d6.chunk.js"
+  },
+  {
+    "revision": "bfcea4cde917b31e85bc",
+    "url": "/react-todo-app/static/js/runtime-main.7961473d.js"
+  }
+]);
