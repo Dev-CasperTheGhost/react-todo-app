f33b3ee274b7fd5d359388502ddb905311e5967c
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-05-05 15:46:50 +0200

    added build for github page

---
+++ build/precache-manifest.19ee33a21512022a0307c1c771d62a5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e42edcca14961492f3f3e86c81798754",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "1a19c896f97f92871ebd",
+    "url": "/react-todo-app/static/css/main.19038ebe.chunk.css"
+  },
+  {
+    "revision": "8c69853f0e5bdf495167",
+    "url": "/react-todo-app/static/js/2.3a011d80.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo-app/static/js/2.3a011d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a19c896f97f92871ebd",
+    "url": "/react-todo-app/static/js/main.6a35786a.chunk.js"
+  },
+  {
+    "revision": "bfcea4cde917b31e85bc",
+    "url": "/react-todo-app/static/js/runtime-main.7961473d.js"
+  }
+]);
